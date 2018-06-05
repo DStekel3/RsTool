155fecbc1bae85a4243554017adeb7e03efa6600
D.J. Stekelenburg <d.j.stekelenburg@students.uu.nl>
2018-06-05 20:02:23 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿using System.Linq;
+
+namespace howto_move_click_mouse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.RowStart = new System.Windows.Forms.TextBox();
+            this.ColStart = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SpeedBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(161, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(694, 108);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Empty my Bag!";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Empty bag";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // RowStart
+            // 
+            this.RowStart.Location = new System.Drawing.Point(210, 29);
+            this.RowStart.Name = "RowStart";
+            this.RowStart.Size = new System.Drawing.Size(100, 20);
+            this.RowStart.TabIndex = 2;
+            this.RowStart.Text = "1";
+            // 
+            // ColStart
+            // 
+            this.ColStart.Location = new System.Drawing.Point(210, 55);
+            this.ColStart.Name = "ColStart";
+            this.ColStart.Size = new System.Drawing.Size(100, 20);
+            this.ColStart.TabIndex = 3;
+            this.ColStart.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(140, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Start at row:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(123, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Start at column:";
+            // 
+            // SpeedBox
+            // 
+            this.SpeedBox.Location = new System.Drawing.Point(210, 81);
+            this.SpeedBox.Name = "SpeedBox";
+            this.SpeedBox.Size = new System.Drawing.Size(100, 20);
+            this.SpeedBox.TabIndex = 6;
+            this.SpeedBox.Text = "100";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(123, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Speed in ms:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.SpeedBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ColStart);
+            this.Controls.Add(this.RowStart);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RunesCape Auto-Clicker";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox RowStart;
+        private System.Windows.Forms.TextBox ColStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox SpeedBox;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Runtime.InteropServices;
+using System.Drawing.Drawing2D;
+using System.Threading;
+using Timer = System.Windows.Forms.Timer;
+
+namespace howto_move_click_mouse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        // See http://www.pinvoke.net/default.aspx/user32/mouse_event.html
+        // Mouse event flags.
+        [Flags]
+        public enum MouseEventFlags : uint
+        {
+            LEFTDOWN = 0x00000002,
+            LEFTUP = 0x00000004,
+            MIDDLEDOWN = 0x00000020,
+            MIDDLEUP = 0x00000040,
+            MOVE = 0x00000001,
+            ABSOLUTE = 0x00008000,
+            RIGHTDOWN = 0x00000008,
+            RIGHTUP = 0x00000010,
+            WHEEL = 0x00000800,
+            XDOWN = 0x00000080,
+            XUP = 0x00000100
+        }
+
+        // Use the values of this enum for the 'dwData' parameter
+        // to specify an X button when using MouseEventFlags.XDOWN or
+        // MouseEventFlags.XUP for the dwFlags parameter.
+        public enum MouseEventDataXButtons : uint
+        {
+            XBUTTON1 = 0x00000001,
+            XBUTTON2 = 0x00000002
+        }
+
+        [DllImport("user32.dll")]
+        static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, int dwExtraInfo);
+
+        // The mouse's target location.
+        private Point m_Target = new Point(150, 100);
+
+        // Move the mouse and click it.
+        private void btnMoveClick_Click(object sender, EventArgs e)
+        {
+            // Convert the target to absolute screen coordinates.
+            Point pt = this.PointToScreen(m_Target);
+
+            // mouse_event moves in a coordinate system where
+            // (0, 0) is in the upper left corner and
+            // (65535,65535) is in the lower right corner.
+            // Convert the coordinates.
+            Rectangle screen_bounds = Screen.GetBounds(pt);
+            uint x = (uint)(pt.X * 65535 / screen_bounds.Width);
+            uint y = (uint)(pt.Y * 65535 / screen_bounds.Height);
+
+            // Move the mouse.
+            mouse_event(
+                (uint)(MouseEventFlags.ABSOLUTE | MouseEventFlags.MOVE),
+                x, y, 0, 0);
+
+            // Click there.
+            mouse_event(
+                (uint)(MouseEventFlags.ABSOLUTE | MouseEventFlags.MOVE |
+                    MouseEventFlags.LEFTDOWN | MouseEventFlags.LEFTUP),
+                x, y, 0, 0);
+        }
+
+        private int sleeper = 100;
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            //var mineSession = new Stopwatch();
+            //while (mineSession.Elapsed.Minutes < 60)
+            //{
+            //    var t = new Stopwatch();
+            //    t.Start();
+            //    while(t.Elapsed.Minutes < 4)
+            //    {
+            //        AutoClickMidScreen();
+            //        Thread.Sleep(3000);
+            //    }
+            //    EmptyBag();
+            //}
+
+
+            // EmptyBag();
+
+        }
+
+        private void EmptyBag(int rowBegin, int colBegin)
+        {
+            Point startPos = new Point(-450 + 40 * (colBegin - 1), 730 + 40 * (rowBegin - 1));
+            // Convert the target to absolute screen coordinates.
+            Point pt = this.PointToScreen(startPos);
+
+            for(int col = 0; col <= (3 - (colBegin - 1)); col++)
+            {
+                pt.X += col * 40;
+                for(int row = 0; row < (7 - (rowBegin - 1)); row++)
+                {
+                    RightMouseClick(pt, true);
+
+                    if(row < (6-(rowBegin - 1)))
+                    {
+                        pt.Y += 40;
+                    }
+
+                    LeftMouseClick(pt, true);
+                }
+                pt = this.PointToScreen(startPos);
+            }
+        }
+
+        private void AutoClickMidScreen()
+        {
+            Point pt = this.PointToScreen(new Point((-1920 / 2) - 20, 1080 / 2));
+            Rectangle screen_bounds = Screen.GetBounds(pt);
+            uint x = (uint)(pt.X * 65535 / screen_bounds.Width);
+            uint y = (uint)(pt.Y * 65535 / screen_bounds.Height);
+
+            // Move the mouse.
+            mouse_event(
+                (uint)(MouseEventFlags.ABSOLUTE | MouseEventFlags.MOVE),
+                x, y, 0, 0);
+
+            mouse_event((uint)(MouseEventFlags.ABSOLUTE | MouseEventFlags.MOVE |
+                               MouseEventFlags.LEFTDOWN | MouseEventFlags.LEFTUP),
+                x, y, 0, 0);
+        }
+
+        private void RightMouseClick(Point pt, bool click)
+        {
+            Rectangle screen_bounds = Screen.GetBounds(pt);
+            uint x = (uint)(pt.X * 65535 / screen_bounds.Width);
+            uint y = (uint)(pt.Y * 65535 / screen_bounds.Height);
+
+            // Move the mouse.
+            mouse_event(
+                (uint)(MouseEventFlags.ABSOLUTE | MouseEventFlags.MOVE),
+                x, y, 0, 0);
+
+            Thread.Sleep(sleeper);
+
+            if(click)
+            {
+                mouse_event((uint)(MouseEventFlags.ABSOLUTE | MouseEventFlags.MOVE |
+                                    MouseEventFlags.RIGHTDOWN | MouseEventFlags.RIGHTUP),
+                    x, y, 0, 0);
+            }
+        }
+
+        private void LeftMouseClick(Point pt, bool click)
+        {
+            Rectangle screen_bounds = Screen.GetBounds(pt);
+            uint x = (uint)(pt.X * 65535 / screen_bounds.Width);
+            uint y = (uint)(pt.Y * 65535 / screen_bounds.Height);
+
+            // Move the mouse.
+            mouse_event(
+                (uint)(MouseEventFlags.ABSOLUTE | MouseEventFlags.MOVE),
+                x, y, 0, 0);
+
+            Thread.Sleep(sleeper);
+            if(click)
+            {
+                mouse_event((uint)(MouseEventFlags.ABSOLUTE | MouseEventFlags.MOVE |
+                                    MouseEventFlags.LEFTDOWN | MouseEventFlags.LEFTUP),
+                    x, y, 0, 0);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EmptyInventory();
+        }
+
+        void EmptyInventory()
+        {
+            int.TryParse(this.RowStart.Text, out int rowBegin);
+            int.TryParse(this.ColStart.Text, out int colBegin);
+            int.TryParse(this.SpeedBox.Text, out int speed);
+            sleeper = speed;
+            EmptyBag(rowBegin, colBegin);
+
+            this.WindowState = WindowState = FormWindowState.Minimized;
+        }
+
+        private void Form1_KeyPress(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+            else if(e.KeyCode == Keys.Space)
+            {
+                EmptyInventory();
+            }
+        }
+    }
+}
